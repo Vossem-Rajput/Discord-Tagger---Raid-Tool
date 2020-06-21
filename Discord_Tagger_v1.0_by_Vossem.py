57a0e71f582e7a5a3be4c4c030467ea7b24631d4
Vossem-Rajput <67219501+Vossem-Rajput@users.noreply.github.com>
2020-06-21 12:49:02 +0530

    Add files via upload
    # Discord-Tagger---Raid-Tool
    This is my Discord Tagger it helps people to Raid Discord Servers and spam Discord DMs. Though this tool is made in Python I converted it to .exe for other users who don't have Python, both are included in this repository as well as my YouTube Channel.
    
    How to use?
    For Normal Users: Run the .exe file in the folder.
    For Advanced users: Type `pip install pyinstaller` in your Command Prompt and run the .py file!
    For .exe file visit my YouTube Channel: https://www.youtube.com/channel/UCqdK83lbIf4RDJp-04Qm2Gw?view_as=subscriber
    This is my first every project in Python and I'll introduce it's more versions too!
    
    Happy Raiding!

---
+++ Discord_Tagger_v1.0_by_Vossem.py
@@ -0,0 +1,62 @@
+import pyautogui
+import time
+
+
+print("░█▀▀▄ ─▀─ █▀▀ █▀▀ █▀▀█ █▀▀█ █▀▀▄ 　 ▀▀█▀▀ █▀▀█ █▀▀▀ █▀▀▀ █▀▀ █▀▀█ 　 █▀▀▄ █──█ 　 ░█──░█ █▀▀█ █▀▀ █▀▀ █▀▀ █▀▄▀█")
+print("░█─░█ ▀█▀ ▀▀█ █── █──█ █▄▄▀ █──█ 　 ─░█── █▄▄█ █─▀█ █─▀█ █▀▀ █▄▄▀ 　 █▀▀▄ █▄▄█ 　 ─░█░█─ █──█ ▀▀█ ▀▀█ █▀▀ █─▀─█") 
+print("░█▄▄▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀─▀▀ ▀▀▀─ 　 ─░█── ▀──▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀─▀▀ 　 ▀▀▀─ ▄▄▄█ 　 ──▀▄▀─ ▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀───▀")
+print("")
+print("")
+print("")
+print("Starting the Tagger")
+print("")
+print("")
+print("")
+
+msg = "@everyone @here"
+n = input("How many times you want to Spam the tags? = ")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("Checking the Tagger")
+print("Check Done: No Errors")
+print("Activating the Clock!")
+print("")
+print("")
+print("")
+print("")
+
+seconds = 5
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+for i in range(seconds):
+    print(str(seconds - i) + " Seconds remaining before the Tagging Starts.")
+    time.sleep(1)
+
+print("Starting the Tagger.")
+
+print("Tagger Started")
+
+for i in range(0, int(n)):
+    pyautogui.typewrite(msg + '\n')
+
+print("Tagger has successfully executed it's given task!")
+print("")
+print("")
+print("")
+print("Made by Vossem! Join our Discord Server:https://discord.gg/F8znxtX")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+input("Thanks for using Discord Tagger by Vossem v1.0! Press any key to exit!")
